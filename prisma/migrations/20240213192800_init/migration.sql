70d2048ed1cb2c5d4c60a927ba9895605cfcc27a
Hexvel <dimaabdukarimov1234@gmail.com>
2024-02-15 17:06:41 +0300

    update & fix

--- prisma/migrations/20240213192800_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "rank" INTEGER NOT NULL DEFAULT 1,
-    "fistName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL
-);
